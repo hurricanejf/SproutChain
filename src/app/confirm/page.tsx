39bae77da5df532a427817193a321c2c85624727
hurricanejf <me@jeffreyalden.com>
2025-12-02 00:21:07 -0800

    Confirm update

--- src/app/confirm/page.tsx
+++ src/app/confirm/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 import { Suspense } from "react";
 import ConfirmLogic from "./ConfirmLogic";
 
